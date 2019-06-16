56b42447a57d6ca8fc8ad85b716a71673a4ccf94
TheWildParamecium <perezgarcia_jesus94@hotmail.com>
2019-06-16 17:06:18 +0200

    prueba de navbar e idiomas

--- js/main.js
+++ js/main.js
@@ -1,28 +1,34 @@
 var app = new Vue({
     el: '#app',
     data: {
-      description: "I'm a biochemist that got interest in applying the power of informatics in the field of life, diseases, etc. I'm also an enthusiastic of the japanese culture, history and language.",
-      mostrar: false,
-      hashes: [{
-          id: 1
-      }, {
-          id: 2
-      }]
+      language: "en",
+      description: {
+          en: "I'm a biochemist that got interest in applying the power of informatics in the field of life, diseases, etc. I'm also an enthusiastic of the japanese culture, history and language.",
+          sp: "Soy un bioquímico que está interesado en aplicar el poder de la informática en el campo de la vida, especialmente en la investigación de enfermedades. También soy un gran entusiasta de la cultura japonesa, así como su historia y lenguaje",
+          jp: "私の個人ページのへようこぞう．私はでマラガの大学で生物化学を卒業して現在に生物情報学を働くことを目指しています。それに日本の文化や歴史などに関心を持ているし５年まえから日本語を勉強します"
+      },
+      media: {
+          en: "Find more about me",
+          sp: "Encuentrame en las redes",
+          jp: "私についてを調べて"
+      }
     },
 
     methods: {
-        hola: function() {
-            alert('Hola. soy Oyasu!')
-            this.mostrar = true
+        changeLanguage: function(newlanguage) {
+            this.language = newlanguage
         }
     },
 
     computed: {
-        imprimeFraseRandom: function() {
-            if(!this.mostrar)
-                return 'no hay frase'
-            else
-                return 'PRUKOGIIII'
+        showContent: function() {
+            newLanguage = this.language;
+            return this.description[newLanguage]
+        },
+        showMedia: function() {
+            newLanguage = this.language;
+            return this.media[newLanguage]
         }
     }
-  })
+    
+})
