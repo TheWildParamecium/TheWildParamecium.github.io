bf06a1b6d3e86d1b49818eb29fbd1d080a6fe278
TheWildParamecium <perezgarcia_jesus94@hotmail.com>
2019-06-18 12:24:41 +0200

    minor tipos changed

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var app = new Vue({
       description: {
           en: "I'm a biochemist that got interest in applying the power of informatics in the field of life, diseases, etc. I'm also an enthusiastic of the japanese culture, history and language.",
           sp: "Soy un bioquímico que está interesado en aplicar el poder de la informática en el campo de la vida, especialmente en la investigación de enfermedades. También soy un gran entusiasta de la cultura japonesa, así como su historia y lenguaje",
-          jp: "私の個人ページのへようこぞう．私はでマラガの大学で生物化学を卒業して現在に生物情報学を働くことを目指しています。それに日本の文化や歴史などに関心を持ているし５年まえから日本語を勉強します"
+          jp: "私の個人ページのへようこぞう．私はマラガの大学で生物化学を卒業して現在に生物情報学を働くことを目指しています。それに日本の文化や歴史などに関心を持ているし５年まえから日本語を勉強します"
       },
       media: {
           en: "Find more about me",
